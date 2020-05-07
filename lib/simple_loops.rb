85c124f4264cf1831963fd1bc3881d5b3b85639c
Ted Neben <tedneben@gmail.com>
2020-05-07 02:00:55 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -27,10 +27,10 @@ end
 
 def return_string_array(array)
   counter = 0 
+  new_array = []
   
   while counter < array.length do
-    array.map(&:to_s)
-    puts array
+    new_array.push(array[counter].to_s)
     counter += 1
   end
 end
